8f6d1d61f1a416ddadcbfaa336d6373cfac8bb82
Sunisc <surya.poddutoori@gmail.com>
2025-09-15 17:06:39 -0400

    set up eslint

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,27 +1,39 @@
-import { FaCheck } from "react-icons/fa";
-import { FaX } from "react-icons/fa6";
+import { FaCheck } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
 
 export type ToastState = {
-	visible: boolean;
-	type: 'error' | 'success' | 'none';
-	message: string;
-}
+  visible: boolean;
+  type: 'error' | 'success' | 'none';
+  message: string;
+};
 
 const Toast = ({ toastState }) => {
-	const toastClass = [
-		'toast',
-		!toastState.visible && 'toast-hidden',
-		toastState.type !== 'none' && `toast-${toastState.type}`,
-	].filter(Boolean).join(' ')
+  const toastClass = [
+    'toast',
+    !toastState.visible && 'toast-hidden',
+    toastState.type !== 'none' && `toast-${toastState.type}`,
+  ]
+    .filter(Boolean)
+    .join(' ');
 
-	return (
-		<>
-			<div id="toast" className={toastClass}>
-				{toastState.type === 'success' && <span><FaCheck />{toastState.message}</span>}
-				{toastState.type === 'error' && <span><FaX />{toastState.message}</span>}
-			</div>
-		</>
-	)
-}
+  return (
+    <>
+      <div id="toast" className={toastClass}>
+        {toastState.type === 'success' && (
+          <span>
+            <FaCheck />
+            {toastState.message}
+          </span>
+        )}
+        {toastState.type === 'error' && (
+          <span>
+            <FaX />
+            {toastState.message}
+          </span>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Toast 
+export default Toast;
